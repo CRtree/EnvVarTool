fa33b6ae2a141b38ca5b01db3a926b00b5ea91a0
xiao.zuo <xiao.zuo@amh-group.com>
2025-07-23 17:32:04 +0800

    init repo

---
+++ EnvVarTool/EnvVarListView.swift
@@ -0,0 +1,18 @@
+//
+//  EnvVarListView.swift
+//  EnvVarTool
+//
+//  Created by Samuel on 2025/7/23.
+//
+
+import SwiftUI
+
+struct EnvVarListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnvVarListView()
+}
